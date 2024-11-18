6c3293edd659e26dc2d879f1bd6ae07b0890c29b
Finja <125427419+FinnSnell@users.noreply.github.com>
2024-11-18 18:45:29 +1000

    V2.0
    Replaced regular while loops with game state functions

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import pygame
-import random
-from twister.twister import generate_number
-from twister.twister import generate_randomness
+import sys
+from twister.twister import generate_number, generate_randomness
+
 pygame.init()
 
-#dictionary
-choices = {
-    "rock": {"rock":"draw", "scissors":"win", "paper":"lose"},
-    "paper": {"paper":"draw", "scissors":"lose", "rock":"win"},
-    "scissors": {"scissors":"draw", "paper":"Win", "rock":"lose"}
-}
-aiDict = {
-    1:"rock",
-    2:"scissors",
-    3:"paper"
-}
+aiSelecting = True
+scope = True
+clicked = False
 
-# Screen setup
-screenWidth = 900
+# dictionaries
+choices = {
+    "rock": {"rock": "draw", "scissors": "win", "paper": "lose"},
+    "paper": {"paper": "draw", "scissors": "lose", "rock": "win"},
+    "scissors": {"scissors": "draw", "paper": "win", "rock": "lose"},
+}
+aiDict = {1: "rock", 2: "scissors", 3: "paper"}
+
+# screen
+screenWidth = 1280
 screenHeight = int(screenWidth * 0.5625)
 screen = pygame.display.set_mode((screenWidth, screenHeight))
-pygame.display.set_caption("Main Menu")
 
-# Colours
+# colours
 white = (255, 255, 255)
 black = (0, 0, 0)
 button_col = (255, 0, 0)
@@ -30,18 +29,13 @@ hover_col = (75, 225, 255)
 click_col = (50, 150, 255)
 text_col = (0, 0, 0)
 
-# ranNum = random.randint(1,3)
-# if ranNum == 1:
-#     aiSelection = "rock"
-# if ranNum == 2:
-#     aiSelection = "scissors"
-# if ranNum == 3:
-#     aiSelection = "paper"
-
-# Font
+# font
 font = pygame.font.Font(None, 40)
 
-# Button Class
+# global variables
+playerSelection = None
+aiSelection = None
+
 class Button:
     def __init__(self, x, y, width, height, text):
         self.x = x
@@ -56,20 +50,18 @@ class Button:
         mouse_pos = pygame.mouse.get_pos()
         button_rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-        # Button states
         if button_rect.collidepoint(mouse_pos):
-            if pygame.mouse.get_pressed()[0]:  # Left-click
+            if pygame.mouse.get_pressed()[0]:
                 pygame.draw.rect(screen, click_col, button_rect)
                 clicked = True
             else:
                 pygame.draw.rect(screen, hover_col, button_rect)
-                if clicked:  # Release after clicking
+                if clicked:
                     clicked = False
                     action = True
         else:
             pygame.draw.rect(screen, button_col, button_rect)
 
-        # Render text
         text_img = font.render(self.text, True, text_col)
         text_len = text_img.get_width()
         screen.blit(
@@ -80,103 +72,114 @@ class Button:
 
 
 # buttons
-rock = Button(screenWidth/7, screenHeight/2, 200, 150, "Rock")
-scissors = Button(screenWidth/2.5, screenHeight/2, 200, 150, "Scissors")
-paper = Button(screenWidth/1.5, screenHeight/2, 200, 150, "Paper")
+rock = Button(screenWidth / 7, screenHeight / 2, 200, 150, "Rock")
+scissors = Button(screenWidth / 2.5, screenHeight / 2, 200, 150, "Scissors")
+paper = Button(screenWidth / 1.5, screenHeight / 2, 200, 150, "Paper")
+playAgain = Button(screenWidth / 2.5, screenHeight / 2, 200, 150, "Play Again")
 
-# main loop
-main = True
-game = False
-clicked = False
 
-#ai
-aiSelecting = True
-scope = True
-while main:
-    global playerSelection
-    global aiSelection
-    screen.fill(white)
+class Main:
+    def __init__(self):
+        self.stateManager = StateManager("select")
+        self.select = SelectScene(screen, self.stateManager)
+        self.results = ResultScene(screen, self.stateManager)
+        self.states = {"select": self.select, "result": self.results}
 
-    if aiSelecting:
-        random_numbers = generate_number()
-        aiSelecting = False
-    
-    if scope:
-        randomness = generate_randomness()
-        randomness = int(randomness[0])
-        scope = False
-    
-    if aiSelecting is False and scope is False:
-        aiChoice = random_numbers[randomness]
-        aiSelection = aiDict[aiChoice]
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.states[self.stateManager.get_state()].run()
+            pygame.display.update()
+
+
+class SelectScene:
+    def __init__(self, display, stateManager):
+        self.display = display
+        self.stateManager = stateManager
+
+    def run(self):
+        global playerSelection, aiSelection, aiSelecting, scope
+        global random_numbers, randomness
+        screen.fill(white)
+        aiSelecting = True
+        scope = True
+
+        if aiSelecting:
+            random_numbers = generate_number()
+            aiSelecting = False
         
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            main = False
-
-    if rock.draw_button():
-        playerSelection = "rock"
-        game = True
-        main = False
-    if scissors.draw_button():
-        playerSelection = "scissors"
-        game = True
-        main = False
-    if paper.draw_button():
-        playerSelection = "paper"
-        game = True
-        main = False
-
-    pygame.display.flip()
-
-while game:
-    screen.fill(white)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game = False
-
-    #Better dictionary win handler
-    win = choices[playerSelection][aiSelection]
-
-    # OBSELETE WIN HANDLER
-    # if playerSelection == "rock":
-    #     if aiSelection == "rock":
-    #         win = "draw"
-    #     elif aiSelection == "scissors":
-    #         win = "win"
-    #     elif aiSelection == "paper":
-    #         win = "lose"
-    # elif playerSelection == "paper":
-    #     if aiSelection == "paper":
-    #         win = "draw"
-    #     elif aiSelection == "rock":
-    #         win = "win"
-    #     elif aiSelection == "scissors":
-    #         win = "lose"
-    # elif playerSelection == "scissors":
-    #     if aiSelection == "scissors":
-    #         win = "draw"
-    #     elif aiSelection == "paper":
-    #         win = "win"
-    #     elif aiSelection == "rock":
-    #         win = "lose"
-
-    if win == "draw":
-        endText = ("You Selected ", playerSelection, "Your Opponent Selected", aiSelection, "The Game Is A Draw.")
-    elif win == "lose":
-        endText = ("You Selected ", playerSelection, "Your Opponent Selected", aiSelection, "The Game Is A Loss.")
-    elif win == "win":
-        endText = ("You Selected ", playerSelection, "Your Opponent Selected", aiSelection, "The Game Is A Win.")
-    else:
-        print('error')
-
-    finalText = font.render(str(endText), True, black)
-    textRect = finalText.get_rect()
-    textRect.center = (screenHeight/2, screenWidth/3)
-    screen.blit(finalText, textRect)
-
-    pygame.display.flip()
-    
+        if scope:
+            randomness = generate_randomness()
+            randomness = int(randomness[0])
+            scope = False
         
-pygame.quit()
+        if aiSelecting is False and scope is False:
+            aiChoice = random_numbers[randomness]
+            aiSelection = aiDict[aiChoice]
+
+        #buttons
+        if rock.draw_button():
+            playerSelection = "rock"
+            self.stateManager.set_state("result")
+        if scissors.draw_button():
+            playerSelection = "scissors"
+            self.stateManager.set_state("result")
+        if paper.draw_button():
+            playerSelection = "paper"
+            self.stateManager.set_state("result")
+
+
+class ResultScene:
+    def __init__(self, display, stateManager):
+        self.display = display
+        self.stateManager = stateManager
+
+    def run(self):
+        global playerSelection, aiSelection
+        screen.fill(white)
+
+        # determine result
+        win = choices[playerSelection][aiSelection]
+        if win == "draw":
+            endText = ("You Selected ", playerSelection, "Your Opponent Selected", aiSelection, "The Game Is A Draw.")
+        elif win == "lose":
+            endText = ("You Selected ", playerSelection, "Your Opponent Selected", aiSelection, "The Game Is A Loss.")
+        elif win == "win":
+            endText = ("You Selected ", playerSelection, "Your Opponent Selected", aiSelection, "The Game Is A Win.")
+        else:
+            print('error')
+
+        
+        finalText = font.render(str(endText), True, black)
+        textRect = finalText.get_rect()
+        textRect.center = (screenHeight/1.2, screenWidth/5)
+        screen.blit(finalText, textRect)
+
+        if playAgain.draw_button():
+            self.stateManager.set_state("select")
+
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+
+class StateManager:
+    def __init__(self, currentState):
+        self.currentState = currentState
+
+    def get_state(self):
+        return self.currentState
+
+    def set_state(self, state):
+        self.currentState = state
+
+
+if __name__ == "__main__":
+    runn = Main()
+    runn.run()
